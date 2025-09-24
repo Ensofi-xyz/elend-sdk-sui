d23fca26af65f0d20abde826218b07bc5bff8806
Tittx <datnt@pando-infinity.com>
2025-09-24 10:30:42 +0700

    update sdk

--- src/client.ts
+++ src/client.ts
@@ -1,23 +1,33 @@
 import { SuiClient } from "@mysten/sui/client";
 import { NetworkConfig } from "./interfaces/config";
 import { Reserve } from "./types/object";
+import { IBorrowElendMarketOperation, IDepositElendMarketOperation, IElendMarketCalculationOperation, IElendMarketQueryOperation, IElendMarketRewardOperation, IRepayElendMarketOperation, IWithdrawElendMarketOperation } from "./interfaces/operations";
 
-export abstract class ElendClient {
+export class ElendClient {
   public readonly networkConfig: NetworkConfig;
   public readonly suiClient: SuiClient;
 
   public obligationOwner: string | null;
-  public associateReservesObligation: Map<string, Reserve>();
+  public associateReservesObligation: Map<string, Reserve>;
+
+  public depositOperation: IDepositElendMarketOperation;
+  public borrowOperation: IBorrowElendMarketOperation;
+  public withdrawOperation: IWithdrawElendMarketOperation;
+  public repayOperation: IRepayElendMarketOperation;
+
+  public rewardOperation: IElendMarketRewardOperation;
+  public queryOperation: IElendMarketQueryOperation;
+  public calculationOperation: IElendMarketCalculationOperation;
 
   constructor () {
 
   }
 
-  abstract async refreshObligation(): Promise<void>;
-  abstract async refreshReserves(): Promise<void>;
+  refreshObligation(): Promise<void>;
+  refreshReserves(): Promise<void>;
 
-  abstract async deposit(): Promise<void>;
-  abstract async borrow(): Promise<void>;
-  abstract async withdraw(): Promise<void>;
-  abstract async repay(): Promise<void>;
+  deposit(): Promise<void>;
+  borrow(): Promise<void>;
+  withdraw(): Promise<void>;
+  repay(): Promise<void>;
 }
