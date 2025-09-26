287fffdbee7cd79e51e23fd7abab4ee67c9657c4
Tittx <datnt@pando-infinity.com>
2025-09-26 17:41:57 +0700

    update testing withdraw

---
+++ test/withdraw.ts
@@ -0,0 +1,31 @@
+import { isNil } from 'lodash';
+
+import { SuiClient } from '@mysten/sui/client';
+
+import { ConfigLoader } from '../src/core';
+import { ElendClient } from '../src/elend-client';
+import { Network } from '../src/types/common';
+import { waitSignAndExecuteTransactionIX } from '../src/utils';
+import { getSignerByPrivateKey, getSuiClientInstance } from '../src/utils/sui-client';
+
+const rpcUrl = 'https://fullnode.testnet.sui.io:443';
+const wsRpcUrl = 'https://fullnode.testnet.sui.io:443';
+
+const signer = getSignerByPrivateKey('suiprivkey1qpf275dj6muyampyc5emgyrslt9whguk2mvurn3am9x7yr5937yzkjaafxy');
+const withdrawReserve = '0xa50848b8ea74455f810fc882fd1c309a07fde5ed6022488d3fb97cfa1c790c00'; //USDC
+const withdrawAmount = 10 * 10 ** 6;
+
+const withdraw = async () => {
+  const suiClient = getSuiClientInstance(rpcUrl, wsRpcUrl);
+  const elendClient = await ElendClient.create(Network.Testnet, {
+    obligationOwner: signer.toSuiAddress(),
+    suiClient,
+    isLoadReserves: true,
+  });
+  const tx = await elendClient.withdraw(withdrawReserve, withdrawAmount);
+
+  const withdrawRes = await waitSignAndExecuteTransactionIX(suiClient, tx, signer);
+  console.log('deposit res: ', withdrawRes);
+};
+
+withdraw();
